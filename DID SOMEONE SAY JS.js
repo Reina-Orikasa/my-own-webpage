274a86d6057c4e83dcf9e9cbbb0339eb30a1718e
Reina Orikasa <40137282+Reina-Orikasa@users.noreply.github.com>
2018-07-07 22:16:42 -0700

    test id changed

--- DID SOMEONE SAY JS.js
+++ DID SOMEONE SAY JS.js
@@ -1,2 +1,5 @@
-document.getElementById("test").innerHTML = "NOT A TEST";
+var time = new Date();
 
+var month = time.getMonth() + 1;
+var day = time.getDate();
+document.getElementById('test').innerHTML = "Today's date is: " + month + "/" + day + " and the time is: " + time.getHours() + ":" + time.getMinutes();
