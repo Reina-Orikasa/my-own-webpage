b5de518e5461f5733d2dfa5f2dbb5fb36ff78413
Reina Orikasa <40137282+Reina-Orikasa@users.noreply.github.com>
2018-07-09 21:43:01 -0700

    leading zero fixed

--- DID SOMEONE SAY JS.js
+++ DID SOMEONE SAY JS.js
@@ -1,5 +1,11 @@
 var time = new Date();
 
 var month = time.getMonth() + 1;
+
 var day = time.getDate();
-document.getElementById('test').innerHTML = "Today's date is: " + month + "/" + day + " and the time is: " + time.getHours() + ":" + time.getMinutes();
+
+var hour = ('0' + time.getHours()).slice(-2);
+
+var minute = mins = ('0'+time.getMinutes()).slice(-2);
+
+document.getElementById('test').innerHTML = "Today's date is: " + month + "/" + day + " and the time is: " + hour + ":" + minute;
