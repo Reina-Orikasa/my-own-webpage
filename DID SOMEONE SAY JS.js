a102e4e322eae0333fb226d8cd98921de18e66fd
Reina Orikasa <40137282+Reina-Orikasa@users.noreply.github.com>
2018-08-06 00:11:18 -0700

    added another show

--- DID SOMEONE SAY JS.js
+++ DID SOMEONE SAY JS.js
@@ -115,11 +115,11 @@ totalEpisodes.totalSugarLifeAired = parseInt(totalEpisodes.sugarLifeAiredTotal.m
 
 totalEpisodes.episodesBehindSL = totalEpisodes.totalSugarLifeAired - totalEpisodes.currentEpisodesWatchedSL;
 
-if (totalEpisodes.episodesBehind === 0 ) { 
+if (totalEpisodes.episodesBehindSL === 0 ) { 
     document.getElementById('episodesBehindSugarLife').innerHTML = "<b>I am currently no episodes behind!</b>";    
 }
 
-else if (totalEpisodes.episodesBehind < 0){
+else if (totalEpisodes.episodesBehindSL < 0){
     document.getElementById('episodesBehindSugarLife').innerHTML = "<b>I am from the future! I am " +
     Math.abs(totalEpisodes.episodesBehindSL) + " episodes ahead!";
 }
@@ -133,3 +133,31 @@ totalEpisodes.episodesBehindSL + " episodes behind!</b>";
 document.getElementById('totalEpisodesSugarLife').innerHTML = "<b>Total episodes: " + 
 totalEpisodes.happySugarLife +"</b>";
 
+totalEpisodes.OngakuShoujo = 12;
+totalEpisodes.OngakuShoujoFromHtml = document.getElementById('currentlyWatchedOngakuShoujo');
+totalEpisodes.OngakuShoujoFromText = totalEpisodes.OngakuShoujoFromHtml.textContent;
+totalEpisodes.OngakuShoujoWatched = parseInt(totalEpisodes.OngakuShoujoFromText.match(/(\d+)$/)[0], 10);
+
+totalEpisodes.OngakuShoujoAiredHtml = document.getElementById('currentlyAiredOngakuShoujo');
+totalEpisodes.OngakuShoujoAiredText = totalEpisodes.OngakuShoujoAiredHtml.textContent;
+totalEpisodes.OngakuShoujoAired = parseInt(totalEpisodes.OngakuShoujoAiredText.match(/(\d+)$/)[0], 10);
+
+totalEpisodes.OngakuShoujoBehind = totalEpisodes.OngakuShoujoAired - totalEpisodes.OngakuShoujoWatched;
+
+if (totalEpisodes.OngakuShoujoBehind === 0 ) { 
+    document.getElementById('episodesBehindOngakuShoujo').innerHTML = "<b>I am currently no episodes behind!</b>";    
+}
+
+else if (totalEpisodes.OngakuShoujoBehind < 0){
+    document.getElementById('episodesBehindOngakuShoujo').innerHTML = "<b>I am from the future! I am " +
+    Math.abs(totalEpisodes.OngakuShoujoBehind) + " episodes ahead!";
+}
+
+else {
+    document.getElementById('episodesBehindOngakuShoujo').innerHTML = "<b>I am currently " + 
+totalEpisodes.OngakuShoujoBehind + " episodes behind!</b>";
+}
+
+
+document.getElementById('totalEpisodesOngakuShojo').innerHTML = "<b>Total episodes: " + 
+totalEpisodes.OngakuShoujoAired +"</b>";
